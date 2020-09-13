77d1079a2d0e8c0505575301503598b2ecb84a66
xyyx <xyyx@mail.ru>
2020-09-13 04:19:33 +0000

    soong: whitelist updates
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic1732068ccaa4f70c333e7dc51e834e85ddcff40

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -78,6 +78,7 @@ var Configuration = map[string]PathConfig{
 	"brotli":  Allowed,
         "ccache":  Allowed,
 	"cpio":    Allowed,
+        "date":    Allowed,
 	"dd":      Allowed,
 	"diff":    Allowed,
 	"dlv":     Allowed,
